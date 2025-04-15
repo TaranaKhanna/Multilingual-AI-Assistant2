663ce7b93b2bb0927ad2e647022e644e069c6dae
Tarana Khanna <taranakhanna5@gmail.com>
2025-04-15 20:10:43 +0530

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -28,26 +28,45 @@ function App() {
     if (isValid) {
       setTimeout(() => setShowApiStatus(false), 10000);
     }
-  };
+  }; 
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 p-4 top-0">
       <div className="max-w-6xl mx-auto">
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-3xl font-bold text-gray-800">{t('appTitle')}</h1>
-          <div className="flex space-x-4">
-            <div className="w-40">
-              <ModelSelector
-                onModelChange={handleModelChange}
-                currentModel={currentModel}
-              />
+
+        <div className="flex gap-10 items-center mb-12 lg:mb-20 top-0 sticky bg-white shadow-md rounded-lg pr-4 sm:py-2 lg:py-1">
+          <img src="logo.png" alt="logo" className='w-40 h-20 pl-4 py-2' />
+          <div className='flex flex-col sm:flex-row lg:flex-row justify-around w-full gap-5 lg:gap-10'>
+            <h1 className="text-3xl sm:w-full font-bold text-gray-800 text-center">{t('appTitle')}</h1>
+            <div className="hidden sm:flex sm:flex-col gap-2 lg:flex-row space-x-4">
+              <div className="w-40">
+                <ModelSelector
+                  onModelChange={handleModelChange}
+                  currentModel={currentModel}
+                />
+              </div>
+              <div className="w-40">
+                <LanguageSelector
+                  onLanguageChange={handleLanguageChange}
+                  currentLanguage={currentLanguage}
+                />
+              </div>
             </div>
-            <div className="w-40">
-              <LanguageSelector
-                onLanguageChange={handleLanguageChange}
-                currentLanguage={currentLanguage}
-              />
-            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-2 space-x-4 mb-10 sm:hidden justify-center items-center w-full">
+          <div className="w-40">
+            <ModelSelector
+              onModelChange={handleModelChange}
+              currentModel={currentModel}
+            />
+          </div>
+          <div className="w-40">
+            <LanguageSelector
+              onLanguageChange={handleLanguageChange}
+              currentLanguage={currentLanguage}
+            />
           </div>
         </div>
 
